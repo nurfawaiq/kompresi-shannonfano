1c97a3dbf6d4c1423703e9a3bcadb643902bc242
unknown <nurfawaiq@gmail.com>
2019-05-01 11:53:04 +0700

    initial commit

---
+++ src/tes_shannonfano/Tes_shannonfano.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tes_shannonfano;
+
+/**
+ *
+ * @author Faiq Noor
+ */
+public class Tes_shannonfano {
+
+    public static void main(String[] args) {
+
+        String text = "aaaaaaaaaaaaaabbbbbbccccccddddddeeeee";
+        Counter c = new Counter(text);
+
+        System.out.println(c);
+
+    }
+
+}
